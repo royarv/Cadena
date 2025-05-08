150a9c7222201298e62f14e80d9d48d22e5cdb27
royarv <20243rd054@utez.edu.mx>
2025-05-08 11:47:16 -0600

    Se creo un archivo

---
+++ Cadena.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Cadena{
+    public static void main(String[] arg){
+        //String nombre = "Rodrigo";
+        //System.out.println("Nombre: " + nombre);
+        Scanner almacenar = new Scanner(System.in);
+        System.out.println("Escribe tu nombre");
+        String nombre = almacenar.nextLine();
+
+        System.out.println("Tu nombre es: " +nombre);
+    }
+}
